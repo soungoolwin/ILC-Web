ffe37c1a6d2f2b9f15c2bb035603da369467d37c
Kyaw Swar Hein <kyawswarhein555225@gmail.com>
2025-09-28 23:15:00 +0700

    Newsletter update

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -5,10 +5,21 @@
 </header>
 <body class="flex justify-evenly items-center flex-col">
         <div class="flex flex-col items-center justify-center sm:w-full lg:w-3/4 mx-auto bg-purple-100 p-4 shadow-md mt-2 mb-3 rounded-lg px-2">
-            <p> Download September Newsletter <a href="{{ asset('images/News_September 2025.pdf') }}" download class="text-blue-600 underline mb-2 mt-2 hover:scale-105">
+            <p> Download October Newsletter <a href="{{ asset('images/News_October 2025.pdf') }}" download class="text-blue-600 underline mb-2 mt-2 hover:scale-105">
                 Here
             </a></p>
-            <embed src="{{ asset('images/News_September 2025.pdf') }}#toolbar=0" type="application/pdf" width="100%" height="600px" class="mt-2 rounded shadow" />
+            <embed src="{{ asset('images/News_October 2025.pdf') }}#toolbar=0" type="application/pdf" width="100%" height="600px" class="mt-2 rounded shadow" />
+        </div>
+        <div class="flex flex-col items-center justify-center sm:w-full lg:w-3/4 mx-auto bg-purple-100 p-4 shadow-md mt-2 mb-3 rounded-lg px-2">
+            <p class="font-thin text-4xl mb-2 bg-white-200"> Download All Newsletters </p>
+            <div class="border-t border-gray-300 w-full my-4"></div>
+            <p class="font-thin text-lg mb-2"><a href="{{ asset('images/News_September 2025.pdf') }}" download class="text-blue-600 underline mb-2 mt-2 hover:scale-105"> 
+                RSUGlobal! Newsletter Issue ( 001 ) - September 2025
+            </a></p>
+            <p class="font-thin text-lg mb-2"><a href="{{ asset('images/News_October 2025.pdf') }}" download class="text-blue-600 underline mb-2 mt-2 hover:scale-105"> 
+                RSUGlobal! Newsletter Issue ( 002 ) - October 2025
+            </a></p>
+            </div>
         </div>
 </body>
     @else
